1f932d1ada7d860282de37ecc91ba1ae0c9173de
Martina Boccuzzi <martybb01@gmail.com>
2024-09-02 19:15:08 +0200

    add this->

--- module01/ex03/HumanA.cpp
+++ module01/ex03/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 18:53:58 by marboccu          #+#    #+#             */
-/*   Updated: 2024/09/02 19:04:26 by marboccu         ###   ########.fr       */
+/*   Updated: 2024/09/02 19:12:44 by marboccu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ HumanA::~HumanA() {};
 
 void HumanA::attack() const 
 {
-	std::cout << _name << " attacks with their " << _weapon.getType() << std::endl;
+	std::cout << this->_name << " attacks with their " << this->_weapon.getType() << std::endl;
 }
--- module01/ex03/HumanB.cpp
+++ module01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 18:57:44 by marboccu          #+#    #+#             */
-/*   Updated: 2024/09/02 19:04:22 by marboccu         ###   ########.fr       */
+/*   Updated: 2024/09/02 19:12:36 by marboccu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ HumanB::~HumanB() {};
 void HumanB::attack() const
 {
 	if (_weapon)
-		std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
 	else
-		std::cout << _name << " attacks with his bare hands, so powerful!" << std::endl;
+		std::cout << this->_name << " attacks with his bare hands, so powerful!" << std::endl;
 }
 
 void HumanB::setWeapon(Weapon &weapon) 
 {
-	_weapon = &weapon;
+	this->_weapon = &weapon;
 }
